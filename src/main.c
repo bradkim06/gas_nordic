1a9f0a05ece5c8031adaa024f7b3dbda25d40883
bradkim06 <bradkim06@gmail.com>
2023-09-12 11:42:10 +0900

    Feat:Log: 모듈 추가

--- src/main.c
+++ src/main.c
@@ -7,9 +7,12 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 int main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	LOG_INF("Hello World! %s\n", CONFIG_BOARD);
 	return 0;
 }
