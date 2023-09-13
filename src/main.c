2bcff26616ac2dd7da4c0ef997c20c9c33d0d21a
bradkim06 <bradkim06@gmail.com>
2023-09-13 10:35:40 +0900

    Feat:adc: battery mon,o2,gas add
    - battery monitoring voltage & percentage
    - o2, gas adc input

--- src/main.c
+++ src/main.c
@@ -6,13 +6,20 @@
  * @date 2023-09-12
  */
 
+#include <errno.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(MAIN, LOG_LEVEL_DBG);
 
 int main(void)
 {
 	LOG_INF("Hello World! %s\n", CONFIG_BOARD);
+
+	while (true) {
+		LOG_INF("main thread");
+		k_msleep(60 * MSEC_PER_SEC);
+	}
+
 	return 0;
 }
