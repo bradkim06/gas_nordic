1ac48b0cbf56d456882f70ef394ce2e3a14ad624
bradkim06 <bradkim06@gmail.com>
2023-10-13 11:33:35 +0900

    Feat: bme680 driver Out of Tree

--- src/bme680_app.c
+++ src/bme680_app.c
@@ -6,10 +6,10 @@
  * @date 2023-09-18
  */
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/drivers/gpio.h>
 
-#include "bme680.h"
+#include "bme680_app.h"
 
 /* size of stack area used by each thread */
 #define STACKSIZE 1024
@@ -23,14 +23,13 @@ struct bme680 bme680_result;
 
 static void bme680_mon(void)
 {
-	const struct device *const dev = DEVICE_DT_GET_ONE(bosch_bme680);
-	struct bme680 *p = &bme680_result;
-
+	const struct device *const dev = DEVICE_DT_GET_ONE(bosch_bme68x);
 	if (!device_is_ready(dev)) {
 		LOG_ERR("device not ready.");
 		return;
 	}
 
+	struct bme680 *p = &bme680_result;
 	LOG_INF("Device %s ready", dev->name);
 
 	while (1) {
@@ -42,7 +41,7 @@ static void bme680_mon(void)
 		sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &p->humidity);
 		sensor_channel_get(dev, SENSOR_CHAN_GAS_RES, &p->gas_res);
 
-		LOG_DBG("T: %d.%06d; P: %d.%06d; H: %d.%06d; G: %d.%06d", p->temp.val1,
+		LOG_INF("T: %d.%06d; P: %d.%06d; H: %d.%06d; G: %d.%06d", p->temp.val1,
 			p->temp.val2, p->press.val1, p->press.val2, p->humidity.val1,
 			p->humidity.val2, p->gas_res.val1, p->gas_res.val2);
 	}
