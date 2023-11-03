eafde4ca4c7a3114834138946cf8f95398988dc2
bradkim06 <bradkim06@gmail.com>
2023-11-03 11:32:15 +0900

    Doc: calculate_level_pptt comment ad

--- src/hhs_math.h
+++ src/hhs_math.h
@@ -72,9 +72,47 @@ struct level_point {
  * This function takes a measured ADC voltage level and a curve for the type of level_point as
  * input. It then calculates and returns the estimated curved pptt.
  *
+ * This function, `calculate_level_pptt`, is used to calculate a level (in pptt) based on a given
+ * voltage (in mV) and a curve of points. The curve of points is represented as an array of
+ * `level_point` structures, where each point has a voltage level (`lvl_mV`) and a corresponding
+ * pptt level (`lvl_pptt`).
+ *
+ * Here's a breakdown of the function:
+ *
+ * 1. The function starts by setting `currentPoint` to the start of the `curvePoints` array.
+ *
+ * 2. It then checks if the given `voltage_mV` is greater than or equal to the voltage level of the
+ * `currentPoint`. If it is, it returns the pptt level of the `currentPoint`. This is because
+ * thegiven voltage is above the highest point in the curve, so the function caps the result at the
+ * maximum pptt level.
+ *
+ * 3. If the given voltage is not above the highest point, the function enters a while loop. This
+ * loop iterates over the `curvePoints` array until it finds the last point that is at or below the
+ * given voltage. It does this by incrementing `currentPoint` as long as the pptt level of the
+ * `currentPoint` is greater than 0 and the given voltage is less than the voltage level of the
+ * `currentPoint`.
+ *
+ * 4. After the loop, the function checks if the given voltage is less than the voltage level of the
+ * `currentPoint`. If it is, it returns the pptt level of the `currentPoint`. This is because the
+ * given voltage is below the lowest point in the curve, so the function caps the result at the
+ * minimum pptt level.
+ *
+ * 5. If the given voltage is not below the lowest point, the function performs linear interpolation
+ * to calculate the pptt level corresponding to the given voltage. It does this by finding the
+ * `previousPoint` (the point before the `currentPoint`), and then using the formula for linear
+ * interpolation to calculate the pptt level.
+ * The function then returns this calculated pptt level.
+ *
+ * In summary, this function is used to find the pptt level corresponding to a given voltage, based
+ * on a curve of points. It does this by either capping at the maximum or minimum pptt level (if the
+ * voltage is above the highest point or below the lowest point), or by performing linear
+ * interpolation between the points below and above the given voltage.
+ *
  * @param voltage_mV The measured ADC voltage level in millivolts.
  * @param curvePoints Pointer to the curve for the type of level_point.
  *
  * @return The estimated curved pptt as an unsigned integer.
  */
 unsigned int calculate_level_pptt(unsigned int voltage_mV, const struct level_point *curvePoints);
+
+#endif // __APP_HHS_MATH_H__
