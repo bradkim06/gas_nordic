b0a5b80355612bdadf947aa09167b2225e02134a
bradkim06 <bradkim06@gmail.com>
2023-11-06 17:45:32 +0900

    sh: Reformat code to improve readability
    - This commit removes an unnecessary comment line in flash.sh that was
    previously used for testing.
    - The code in format.sh has been reformatted to improve readability. The
    changes include adjusting indentation and reorganizing the code for
    clarity. No functional changes have been made.

--- flash.sh
+++ flash.sh
@@ -3,4 +3,3 @@
 nrfjprog --version
 nrfjprog -f nrf52 --chiperase --reset --verify --program ./build/zephyr/zephyr.hex
 # nrfjprog --program /Users/bradkim06/hhs/docker/work/gas_ces/build/zephyr/zephyr.hex --chiperase --verify -f NRF52
-
